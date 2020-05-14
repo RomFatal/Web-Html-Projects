344f24bcbb922c5940d43985401ca3c1019fc099
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2020-05-15 01:03:16 +0300

    Final fixes

--- ex3/includes/script.js
+++ ex3/includes/script.js
@@ -12,12 +12,8 @@ return p.firstName.length * p.lastName.length
 (function () {
     numOfRectangle=calcName(person);
     for (let index = 0; index < numOfRectangle; index++) {
-    var main = document.getElementById('lay2Main');
+    var main = document.getElementById('boxContainer');
     var rec = document.createElement('article');
-    rec.style.height = '312px';
-    rec.style.width = '204px';
-    rec.style.marginTop = '48px';
-    rec.style.cssFloat = 'Left';
     main.appendChild(rec)
  }
 })();
@@ -28,18 +24,20 @@ document.querySelector('#paint').addEventListener('click', function (e) {
         rec[index].style.backgroundColor=('gray')  
     }
     colorState = 1;
-    });
+});
+    
     document.querySelector('#clear-btn').addEventListener('click', function (e) {
         const rec = document.getElementsByTagName('article');
         for (let  index= 0;  index< person.firstName.length; index++) {
             rec[index].style.backgroundColor=('white')  
         }
         colorState = 0;
-    });
+});
+
 var firstRec = document.querySelector('article');
 firstRec.addEventListener('mouseover', function (e) {
-    letter = person.firstName.charAt(0)
-    firstRec.appendChild(document.createTextNode(letter))
+    
+    firstRec.appendChild(document.createTextNode(person.firstName.charAt(0)))
     firstRec.style.fontSize = '270px';
     firstRec.style.fontWeight = ('bold');
     firstRec.style.color = ('white');
