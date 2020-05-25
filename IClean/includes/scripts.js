91a9ac3332900a531de177c328c2972dd331f772
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2020-05-25 05:40:19 +0300

    Add User Flow

--- IClean/includes/scripts.js
+++ IClean/includes/scripts.js
@@ -15,7 +15,30 @@ $(document).ready(function () {
                     '<td>' + tableCol[1].textContent + '</td>' +
                     '<td>' + tableCol[2].textContent + '</td>' +
                     '<td>' + tableCol[3].textContent + '</td>' +
-                    '<td>' + '<a href="garbageInfo.html"><span class="material-icons navbar-brand">edit</span><span class="material-icons navbar-brand">visibility</span><span class="material-icons navbar-brand">delete_forever</span></a>' + '</td>' +
+                    '<td>' + '<a href="garbageInfo.html"><span class="material-icons navbar-brand">edit</span></a><a href="garbageInfo.html"><span class="material-icons navbar-brand">visibility</span></a><a href="garbageInfo.html"><span class="material-icons navbar-brand">delete_forever</span></a>' + '</td>' +
+                    '</tr>'
+            }
+        });
+        $('tbody').html(match)
+        modifyTable = document.querySelectorAll('tr');
+    })
+});
+
+$(document).ready(function () {
+    $('#Urgency_user').on('change', function (e) {
+        var value = e.target.value
+        var match = ''
+        table.forEach(row => {
+            tableCol = row.querySelectorAll('td');
+            if (tableCol.length == 0) return;
+            if (tableCol[3].textContent == value || value == "all") {
+                match +=
+                    '<tr>' +
+                    '<td scope="row">' + tableCol[0].textContent + '</td>' +
+                    '<td>' + tableCol[1].textContent + '</td>' +
+                    '<td>' + tableCol[2].textContent + '</td>' +
+                    '<td>' + tableCol[3].textContent + '</td>' +
+                    '<td>' + '<a href="user_garbageInfo.html"><span class="material-icons navbar-brand">edit</span></a><a href="user_garbageInfo.html"><span class="material-icons navbar-brand">visibility</span></a>' + '</td>' +
                     '</tr>'
             }
         });
@@ -62,3 +85,50 @@ $(document).ready(function () {
         $(this).parent().find("span").stop().animate({ marginTop: "1px", opacity: 1 }, 300);
     });
 });
+function usereditgarbage(id)
+{
+      var comment =   document.getElementById('Comment')
+      if(comment.disabled == true){
+        comment.disabled = false;
+        id.innerHTML = "Save";
+      }else{
+        comment.disabled = true;
+        id.innerHTML = "Edit Garbage";
+      }
+      
+      
+}
+function editgarbage(id)
+{
+
+    var comment =   document.getElementById('Comment')
+    var radio1 =   document.getElementById('customRadio1')
+    var radio2 =   document.getElementById('customRadio2')
+    var radio3 =   document.getElementById('customRadio3')
+    var radio4 =   document.getElementById('customRadio4')
+    var capacity =   document.getElementById('Capacity')
+    var address =   document.getElementById('Address')
+    
+    
+      if(comment.disabled == true){
+        comment.disabled = false;
+        radio1.disabled = false;
+        radio2.disabled = false;
+        radio3.disabled = false;
+        radio4.disabled = false;
+        capacity.disabled = false;
+        address.disabled = false;
+        id.innerHTML = "Save";
+      }else{
+        comment.disabled = true;
+        radio1.disabled = true;
+        radio2.disabled = true;
+        radio3.disabled = true;
+        radio4.disabled = true;
+        capacity.disabled = true;
+        address.disabled = true;
+        id.innerHTML = "Edit Garbage";
+      }
+      
+      
+}
